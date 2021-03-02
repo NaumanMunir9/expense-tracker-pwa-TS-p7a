6b17e18d1cbbe8af93a902dff7bb0c0ae2828e9c
Nauman Munir <naumanmunir@gmail.com>
2021-03-02 15:05:33 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./App.styles.scss";
 import AddTransaction from "./components/AddTransaction/AddTransaction.component";
 import Balance from "./components/Balance/Balance.component";
 import Header from "./components/Header/Header.component";
 import IncomeExpense from "./components/IncomeExpense/IncomeExpense.component";
 import TransactionList from "./components/TransactionList/TransactionList.component";
 import GlobalContext from "./context/GlobalContext";
+import "./App.styles.scss";
 
 const App: React.FC = () => {
   return (
